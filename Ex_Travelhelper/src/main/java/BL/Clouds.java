705b9c247a4cafc5420f4e76afbb59673abc9ffb
Franz Backhaus <bacfrb15@htlkaindorf.at>
2020-01-22 09:11:13 +0100

    Initial Commit

---
+++ Ex_Travelhelper/src/main/java/BL/Clouds.java
@@ -0,0 +1,15 @@
+package BL;
+
+public class Clouds {
+
+    private int all;
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+
+}
